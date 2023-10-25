1387584c603222e7ebb556a266b4dcee761a2ee2
init_mumu <bshookhu@gmail.com>
2023-10-25 22:18:17 +0900

    edit HTTPRequest

--- HTTPRequest.py
+++ HTTPRequest.py
@@ -1,25 +1,50 @@
 class Request:
-    def __init__(self, method, host, url, data=None):
+    def __init__(self):
+        self.method = None
+        self.url = None
+        self.version = "HTTP/1.1"
+        self.header = dict()
+
+        self.data = None
+
+    def setRequest(self, method, host, url, data=None):
         self.method = method
         self.url = url
         self.version = "HTTP/1.1"
 
         self.data = data
-        self.encode_data()
 
         self.header = dict()
         self.header["Host"] = host
         self.header["Accept"] = "*/*"
-        self.header["Content-Length"] = self.calc_content_length()
+        self.setContentLength()
+
+    def setHost(self, host):
+        self.header["Host"] = host
+
+    def setURL(self, url):
+        self.url = url
+
+    def setVersion(self, version):
+        self.version = version
+
+    def setData(self, data):
+        self.data = data
+
+    def setHeader(self, key, value):
+        self.header[key] = value
+
+    def setContentLength(self):
+        if self.data is None:
+            return
+        
+        self.header["Content-Length"] = len(self.data)
 
     def encode_data(self):
         if type(self.data) is str:
             self.data = self.data.encode("utf-8")
-
-    def calc_content_length(self):  
-        return len(self.data)
     
-    def get(self):
+    def __str__(self):
         request = ""
         request += f"{self.method} {self.url} {self.version}\r\n"
         
