5da704b42e2fe61b13550a7435fd871e38636383
Alex <dlwnstkd627@gmail.com>
2023-11-17 06:19:54 +0900

    baseHandler add

--- http/BaseHandler_Server.py
+++ http/BaseHandler_Server.py
@@ -1,21 +1,26 @@
 class BaseHandler:
-    def __init__(self):
-        self.router = dict()
-        user = ""
-
-    def registerHandler(self, url, handler):
-        self.router[url] = handler
-
-    def processEvent(self, req):
-        try:
-            handler = self.router[req.url]
-            handler(req)
-        except Exception as e:
-            print(f"Can not find Event Handler for {req.url}")
-            return
         
     def client_handler(self, client_socket):
         print("Client Address: ", client_socket.addr)
         user = client_socket.recv(1024)
         string = "User name :  %s "%user.decode()
         client_socket.sendall(string.encode())
+
+    # def temp1(self):
+
+    # def temp2(self):
+
+    # def __init__(self):
+    #     self.router = dict()
+    #     user = ""
+
+    # def registerHandler(self, url, handler):
+    #     self.router[url] = handler
+
+    # def processEvent(self, req):
+    #     try:
+    #         handler = self.router[req.url]
+    #         handler(req)
+    #     except Exception as e:
+    #         print(f"Can not find Event Handler for {req.url}")
+    #         return
